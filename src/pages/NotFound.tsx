cb25bd2b79910297de42bb8b54616199eeb580da
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-25 13:41:38 -0300

    feat: added NotFound screen to AppRoutes

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,20 @@
+import logo from '../assets/logo.svg'
+
+export function NotFound() {
+  return (
+    <main className="min-h-screen bg-background bg-desktop-dark bg-no-repeat bg-cover flex flex-col justify-center items-center py-6 px-11">
+      <img className="w-full md:w-[20rem]" src={logo} alt="" />
+
+      <span className="text-white text-center text-sm md:text-lg mt-5">
+        Erro 404 - Essa página não foi encontrada
+      </span>
+
+      <a
+        href="/"
+        className="text-green-700 transition-all duration-150 hover:text-green-900 text-center text-sm md:text-lg mt-10"
+      >
+        Volte para o início 🐍
+      </a>
+    </main>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,13 @@ import { Route } from 'react-router'
 import { Routes } from 'react-router-dom'
 
 import { Landing } from './pages/Landing'
+import { NotFound } from './pages/NotFound'
 
 export function AppRouter() {
   return (
     <Routes>
       <Route Component={Landing} path="/" />
+      <Route Component={NotFound} path="*" />
     </Routes>
   )
 }
