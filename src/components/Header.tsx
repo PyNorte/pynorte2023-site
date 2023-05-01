df36027277b9370f46db197d0885eb4f2442088b
Davi <davimoreiraasantoss@gmail.com>
2023-05-01 17:53:34 -0300

    feat: cosmuzited header menu button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,12 @@ import logo from '../assets/logo.svg'
 
 export function Header() {
   return (
-    <header className="bg-transparent p-8">
+    <header className="bg-black p-8">
         <div className="flex justify-between items-center">
             <img src={logo} alt="Python Norte 2023 Logo" className="max-h-16" />
-            <FiMenu className='text-white text-3xl'/>
+            <button className='hover:bg-zinc-800 rounded-2xl w-12 h-12 flex justify-center items-center'>
+              <FiMenu className='text-white text-3xl'/>
+            </button>
         </div>
     </header>
   )
