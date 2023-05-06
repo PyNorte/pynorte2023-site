64619d030b7d7915e7ac58bcdde98c8b6bc27018
Davi <davimoreiraasantoss@gmail.com>
2023-05-06 19:39:58 -0300

    feat: added to header nav bar and made it responsible

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,19 @@ import logo from '../assets/logo.svg'
 
 export function Header() {
   return (
-    <header className="bg-transparent p-8">
+    <header className="bg-black p-8">
         <div className="flex justify-between items-center">
-            <img src={logo} alt="Python Norte 2023 Logo" className="max-h-16" />
-            <button className='hover:bg-zinc-800 rounded-2xl w-12 h-12 flex justify-center items-center'>
+            <img src={logo} alt="Python Norte 2023 Logo" className="max-h-16 lg:mr-52 xl:mr-72 2xl:mr-96"/>
+            <nav className="text-white w-full hidden lg:flex">
+              <ul className="flex justify-between w-full text-base">
+                <li>Home</li>
+                <li>Código de conduta</li>
+                <li>Keynotes</li>
+                <li>Nossa Equipe</li>
+                <li>FAQ</li>
+              </ul>
+            </nav>
+            <button className='lg:hidden  hover:bg-zinc-800 rounded-2xl w-12 h-12 flex justify-center items-center'>
               <FiMenu className='text-white text-3xl'/>
             </button>
         </div>
