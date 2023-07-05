8ae587a53e16999d92562ae3586a6ea3f62f3181
junowoz <junogouvea@gmail.com>
2023-07-05 13:29:16 -0400

    Footer + Header 100%

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import brandImg from '../assets/brand.svg'
 
 export function Footer() {
   return (
-    <footer className="flex items-center justify-between bg-zinc-900 px-8 py-12 md:px-28 lg:px-40">
-      <div className="space-y-8">
+    <footer className="flex flex-col md:flex-row items-start justify-between bg-zinc-900 px-8 py-12 md:px-28 lg:px-40">
+      <div className="space-y-8 md:mr-16">
         <img className="h-10 md:h-16" src={brandImg} alt="Python Norte 2023" />
 
         <p className="max-w-md text-sm text-zinc-600 md:text-base">
@@ -35,7 +35,21 @@ export function Footer() {
         </section>
       </div>
 
-      <div></div>
+      {/* Links Rápidos */}
+      <div className="mt-8 md:mt-6">
+        <h3 className="text-md text-white font-normal mb-4">Links Rápidos</h3>
+        <ul className="space-y-2">
+          <li>
+            <a className="max-w-md text-sm text-zinc-600 md:text-base hover:text-zinc-400" href="#">Link 1</a>
+          </li>
+          <li>
+            <a className="max-w-md text-sm text-zinc-600 md:text-base hover:text-zinc-400" href="#">Link 2</a>
+          </li>
+          <li>
+            <a className="max-w-md text-sm text-zinc-600 md:text-base hover:text-zinc-400" href="#">Link 3</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,103 @@
-import { clsx } from 'clsx'
-
-import { useScrollPosition } from '../hooks/useScrollPosition'
-
-import brandImg from '../assets/brand.svg'
+import React, { useState } from "react";
+import { clsx } from "clsx";
+import { useScrollPosition } from "../hooks/useScrollPosition";
+import { useMediaQuery } from "react-responsive";
+import { Disclosure } from "@headlessui/react";
+import { BiMenu } from "react-icons/bi";
+import { CgClose } from "react-icons/cg";
+import brandImg from "../assets/brand.svg";
 
 export function Header() {
-  const scrollPosition = useScrollPosition()
+  const [isOpen, setIsOpen] = useState(false);
+  const scrollPosition = useScrollPosition();
+  const isMobile = useMediaQuery({ maxWidth: 640 });
 
   return (
-    <header
+    <Disclosure
+      as="header"
       className={clsx(
-        'sticky top-0 z-50 flex items-center justify-between px-8 py-4 sm:bg-zinc-900/40 sm:backdrop-blur-sm md:px-28 lg:px-40',
+        "sticky top-0 z-50 flex flex-col items-center justify-between px-8 py-4 sm:flex-row sm:bg-zinc-900/40 sm:backdrop-blur-sm md:px-28 lg:px-40",
         {
-          'bg-zinc-900/30 backdrop-blur-sm transition-all':
+          "bg-zinc-900/30 backdrop-blur-sm transition-all":
             scrollPosition >= 20,
-        },
+        }
       )}
     >
-      <img className="h-10 md:h-14" src={brandImg} alt="Python Norte 2023" />
+      <div className="flex w-full items-center justify-between">
+        <img
+          className="mr-16 h-10 md:h-14"
+          src={brandImg}
+          alt="Python Norte 2023"
+        />
 
-      <nav></nav>
-    </header>
-  )
+        {isMobile ? (
+          <Disclosure.Button
+            onClick={() => setIsOpen(!isOpen)}
+            className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span className="sr-only">
+              {isOpen ? "Close main menu" : "Open main menu"}
+            </span>
+            {isOpen ? (
+              <CgClose className="block h-6 w-6" aria-hidden="true" />
+            ) : (
+              <BiMenu className="block h-6 w-6" aria-hidden="true" />
+            )}
+          </Disclosure.Button>
+        ) : (
+          <nav>
+            <ul className="flex space-x-8 text-white md:space-x-16 lg:space-x-24">
+              <li>
+                <a href="#" className="hover:text-gray-300">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-gray-300">
+                  Código de Conduta
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-gray-300">
+                  Keynotes
+                </a>
+              </li>
+              <li>
+                <a href="#faq" className="hover:text-gray-300">
+                  FAQ
+                </a>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </div>
+
+      {isMobile && isOpen && (
+        <Disclosure.Panel className="mt-8 w-full sm:hidden">
+          <ul className="flex flex-col space-y-4 text-white">
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Código de Conduta
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Keynotes
+              </a>
+            </li>
+            <li>
+              <a href="#faq" className="hover:text-gray-300">
+                FAQ
+              </a>
+            </li>
+          </ul>
+        </Disclosure.Panel>
+      )}
+    </Disclosure>
+  );
 }
