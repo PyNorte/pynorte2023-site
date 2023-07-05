4b67cc5dbe2a2036d60202fea83a8d99a8256066
junowoz <junogouvea@gmail.com>
2023-07-05 13:30:31 -0400

    cores custom +

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-        mono: ['Share Tech Mono', 'monospace'],
+        sans: ["Poppins", "sans-serif"],
+        mono: ["Share Tech Mono", "monospace"],
       },
       colors: {
         green: {
-          50: '#d8f1cd',
-          100: '#bee8ac',
-          200: '#a3de8b',
-          300: '#89d56a',
-          400: '#6fcc48',
-          500: '#5ab733',
-          600: '#56af31',
-          700: '#49952a',
-          800: '#397421',
-          900: '#295317',
-          950: '#18320e',
+          50: "#d8f1cd",
+          100: "#bee8ac",
+          200: "#a3de8b",
+          300: "#89d56a",
+          400: "#6fcc48",
+          500: "#5ab733",
+          600: "#56af31",
+          700: "#49952a",
+          800: "#397421",
+          900: "#295317",
+          950: "#18320e",
+        },
+        //Cores custom
+        custom: {
+          gray: "#1C1B1F",
+          salmon: "#FF545A",
+          gold: "#FFD700",
+          silver: "#C0C0C0",
+          bronze: "#CD7F32",
         },
       },
     },
   },
   plugins: [],
-}
+};
